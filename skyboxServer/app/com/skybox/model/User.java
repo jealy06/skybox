ea68ea5faed304b42c6e129be552375c57feb589
JIALUN LIU <jl4347@columbia.edu>
2015-05-25 18:44:36 -0400

    The User class initialized according to Users table

---
+++ skyboxServer/app/com/skybox/model/User.java
@@ -0,0 +1,11 @@
+package com.skybox.model;
+
+public class User {
+	public String userName;
+	public String password;
+
+	public User(String userName, String password) {
+		this.userName = userName;
+		this.password = password;
+	}
+}
