e111b430740b64ca0eb35168bc3b61eb2a454591
JIALUN LIU <jl4347@columbia.edu>
2015-05-25 21:37:05 -0400

    Generate Getter and Setter for User and NFLplayerstats class

--- skyboxServer/app/com/skybox/model/NFLplayerstats.java
+++ skyboxServer/app/com/skybox/model/NFLplayerstats.java
@@ -1,20 +1,20 @@
 package com.skybox.model;
 
 public class NFLplayerstats {
-	public int rank;
-    public String playerName;
-    public String team;
-    public int COMP;
-    public int ATT;
-    public float PCT;
-    public int YDS;
-    public float YDS_A;
-    public int LONGP;
-    public int TD;
-    public int INTT;
-    public int SACK;
-    public float RATE;
-    public int YDS_G;
+	private int rank;
+	private String playerName;
+	private String team;
+	private int COMP;
+	private int ATT;
+	private float PCT;
+	private int YDS;
+	private float YDS_A;
+	private int LONGP;
+	private int TD;
+	private int INTT;
+	private int SACK;
+	private float RATE;
+	private int YDS_G;
     
 	public NFLplayerstats(String playerName, String team, int COMP, int ATT,
 			float PCT, int YDS, float YDS_A, int lONGP, int TD, int INTT,
@@ -32,5 +32,117 @@ public class NFLplayerstats {
 		this.SACK = SACK;
 		this.RATE = RATE;
 		this.YDS_G = YDS_G;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+	public int getCOMP() {
+		return COMP;
+	}
+
+	public void setCOMP(int cOMP) {
+		COMP = cOMP;
+	}
+
+	public int getATT() {
+		return ATT;
+	}
+
+	public void setATT(int aTT) {
+		ATT = aTT;
+	}
+
+	public float getPCT() {
+		return PCT;
+	}
+
+	public void setPCT(float pCT) {
+		PCT = pCT;
+	}
+
+	public int getYDS() {
+		return YDS;
+	}
+
+	public void setYDS(int yDS) {
+		YDS = yDS;
+	}
+
+	public float getYDS_A() {
+		return YDS_A;
+	}
+
+	public void setYDS_A(float yDS_A) {
+		YDS_A = yDS_A;
+	}
+
+	public int getLONGP() {
+		return LONGP;
+	}
+
+	public void setLONGP(int lONGP) {
+		LONGP = lONGP;
+	}
+
+	public int getTD() {
+		return TD;
+	}
+
+	public void setTD(int tD) {
+		TD = tD;
+	}
+
+	public int getINTT() {
+		return INTT;
+	}
+
+	public void setINTT(int iNTT) {
+		INTT = iNTT;
+	}
+
+	public int getSACK() {
+		return SACK;
+	}
+
+	public void setSACK(int sACK) {
+		SACK = sACK;
+	}
+
+	public float getRATE() {
+		return RATE;
+	}
+
+	public void setRATE(float rATE) {
+		RATE = rATE;
+	}
+
+	public int getYDS_G() {
+		return YDS_G;
+	}
+
+	public void setYDS_G(int yDS_G) {
+		YDS_G = yDS_G;
 	}
 }
--- skyboxServer/app/com/skybox/model/User.java
+++ skyboxServer/app/com/skybox/model/User.java
@@ -1,11 +1,37 @@
 package com.skybox.model;
 
 public class User {
-	public String userName;
-	public String password;
+	private int userId;
+	private String userName;
+	private String password;
 
-	public User(String userName, String password) {
+	public User(int userId, String userName, String password) {
+		this.userId = userId;
 		this.userName = userName;
+		this.password = password;
+	}
+	
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
 		this.password = password;
 	}
 }
