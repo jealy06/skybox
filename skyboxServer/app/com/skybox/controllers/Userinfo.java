16e80250c0667168392932de8a13323f8c9f8a27
JIALUN LIU <jl4347@columbia.edu>
2015-05-26 18:21:43 -0400

    Controller User class to handle requests to Users table

---
+++ skyboxServer/app/com/skybox/controllers/Userinfo.java
@@ -0,0 +1,45 @@
+package com.skybox.controllers;
+
+import java.util.ArrayList;
+
+import com.skybox.dbmanager.UserDBManager;
+import com.skybox.model.User;
+
+import play.*;
+import play.libs.Json;
+import play.mvc.*;
+import views.html.*;
+
+public class Userinfo extends Controller {
+
+	public static Result getList() {
+		UserDBManager udbm = new UserDBManager();
+		ArrayList<User> userList = new ArrayList<>();
+		
+		udbm.connect();
+		userList = udbm.getList();
+		udbm.closeConnection();
+		
+		return ok(Json.toJson(userList));
+	}
+
+	public static Result getItem(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Result createItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Result updateItem(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Result deleteItem(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
