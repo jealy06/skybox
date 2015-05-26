8b3bf8b490e87c50a76de60fddc4c59173bd1fee
JIALUN LIU <jl4347@columbia.edu>
2015-05-26 18:22:29 -0400

    DBManager Userinfo class to perform actual DB operation

---
+++ skyboxServer/app/com/skybox/dbmanager/DBManager.java
@@ -0,0 +1,21 @@
+package com.skybox.dbmanager;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public interface DBManager {
+
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String DB_URL = "jdbc:mysql://myskyboxdb.cwu2xrntuwr7.us-west-2.rds.amazonaws.com"
+			+ ":3306/NFLDB?user=skyboxplay2015&password=Fantasytakeover68";
+	
+	public void connect();
+	public ArrayList<? extends Object> getList();
+	public Object getItem(int id);
+	public boolean createItem();
+	public boolean updateItem(int id);
+	public boolean deleteItem(int id);
+	
+	public void closeConnection();
+}
