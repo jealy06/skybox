1d30130ba82c33b7dc63068b8904444d88a4d0d0
JIALUN LIU <jl4347@columbia.edu>
2015-05-27 15:47:12 -0400

    Change DBManager to abstract class instead of interface

--- skyboxServer/app/com/skybox/dbmanager/DBManager.java
+++ skyboxServer/app/com/skybox/dbmanager/DBManager.java
@@ -1,25 +1,40 @@
 package com.skybox.dbmanager;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.util.ArrayList;
 
 /*
  * This is the interface of all the database manager, all the DBManager in this package is used
  * to make sql queries to the database and send back the results if available
  */
-public interface DBManager {
+public abstract class DBManager<O> {
 
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
 	static final String DB_URL = "jdbc:mysql://myskyboxdb.cwu2xrntuwr7.us-west-2.rds.amazonaws.com"
 			+ ":3306/NFLDB?user=skyboxplay2015&password=Fantasytakeover68";
 	
-	public void connect();
-	public ArrayList<? extends Object> getList();
-	public Object getItem(int id);
-	public boolean createItem();
-	public boolean updateItem(int id);
-	public boolean deleteItem(int id);
+	public void connect() {
+	}
 	
-	public void closeConnection();
+	public ArrayList<? extends Object> getList() {
+		return null;
+	}
+	
+	public Object getItem(int id) {
+		return null;
+	}
+	
+	public boolean createItem(O entry) {
+		return false;
+	}
+	
+	public boolean updateItem(int id) {
+		return false;
+	}
+	
+	public boolean deleteItem(int id) {
+		return false;
+	}
+	
+	public void closeConnection() {
+	}
 }
